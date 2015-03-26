e893990ebae93fa134ed014b4eb70627ac8ee440
Henning Leutz <leutz@pcsg.de>
2015-03-26 11:33:30 +0100

    ignore .git files, at README

--- setup/quiqqer.php
+++ setup/quiqqer.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of QUIQQER
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 spl_autoload_register(function($className)
 {
     if ( class_exists( $className ) ) {

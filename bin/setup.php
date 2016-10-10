dc765f3bcabfedfc7108e928fca245c8b56a5f05
Michael <michael.danielczok@gmail.com>
2016-10-10 16:53:29 +0200

    feat: unsemantic (testweise --> vllt braucht man doch nicht) feat: setup.php refactor: notwendige requieres in index.php

---
+++ bin/setup.php
@@ -0,0 +1,14 @@
+<div class="page grid-container">
+    <div class="header grid-100">
+        <h1>Dupa</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+
+        <?php
+        /*$version = QUI\Setup\Setup::getVersions();
+        for($i=0; $i<count($version); $i++) {
+            echo $version[i];
+        }*/
+        ?>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 <head>
     <script src="/vendor/quiqqer/qui/qui/lib/mootools-core.js"></script>
     <script src="/vendor/quiqqer/qui/qui/lib/mootools-more.js"></script>
+
+    <link rel="stylesheet" href="/bin/css/font-awesome/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="/bin/css/unsemantic/unsemantic-grid-responsive.css" type="text/css" />
+    <link rel="stylesheet" href="/bin/css/style.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,800' rel='stylesheet' type='text/css'>
+
+    <?php
+    require "vendor/autoload.php";
+    require "bin/setup.php";
+
+    ?>
 </head>
 <body>
 
@@ -13,6 +24,18 @@
         }
     }).send();
 </script>
+
+<?php
+
+echo "test";
+$Locale = new \QUI\Setup\Locale\Locale('de_DE');
+$text = $Locale->getStringLang('setup.message.step.database');
+echo $text;
+
+
+
+?>
+<i class="fa fa-user"></i>
 
 </body>
 </html>

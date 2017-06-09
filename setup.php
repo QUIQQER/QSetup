864eb61e7b57553cef10e496b315f2d2b8cbec89
Florian Bogner <f.bogner@pcsg.de>
2017-06-09 16:29:16 +0200

    feat: Added check for CLI in setup.php. Closes quiqqer/qsetup#47

--- setup.php
+++ setup.php
@@ -4,5 +4,10 @@ namespace QUI\ConsoleSetup;
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
+
+if (php_sapi_name() !== 'cli') {
+    header("Location: /index.php");
+}
+
 $Setup = new Installer();
 $Setup->execute();

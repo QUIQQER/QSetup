84d337e620b055b47888943c4227faaab193931e
michael.daniel <michael.danielczok@gmail.com>
2017-11-09 16:53:27 +0100

    feat: System Check umgebaut. Jetzt werden neue Checks von Flo ausgeführt. feat: Button, der wie ein Step aussieht. Klick öffne ein Popup. Hier werden per Ajax die Tests geholt. feat: Verschiedene Farben und Icons für den System-Check-Button. Dadurch bekommt der Benutzer Info auch bei Warnings.

--- ajax/checkRequirements.php
+++ ajax/checkRequirements.php
@@ -5,25 +5,88 @@ require 'header.php';
 /**
  * This will check the QUIQQER Requirements and return an array with all failed testnames or an empty array if all tests succeeded
  *
+ * @param  lang - User language
+ *
  * @return array
  */
 
-$failedTests = array();
+if (!isset($_REQUEST['lang'])) {
+    $_REQUEST['lang'] = 'en';
+}
 
-$Requirements = \QUI\Requirements\Requirements::runAll();
-/** @var \QUI\Requirements\TestResult $Requirement */
-foreach ($Requirements as $Requirement) {
-    $name = $Requirement['name'];
-    /** @var \QUI\Requirements\TestResult $TestResult */
-    $TestResult = $Requirement['result'];
+$lang                 = substr($_REQUEST['lang'], 0, 2);
+$tests                = array();
+$tests['htmlResult']  = '';
+$tests['testsFailed'] = false;
+$tests['icon']        = 'fa-check';
+$tests['status']      = 'ok';
 
-    if ($TestResult->getStatus() == \QUI\Requirements\TestResult::STATUS_FAILED) {
-        $failedTests[] = $name;
+$Requirements = new \QUI\Requirements\Requirements($lang);
+
+$allTests = $Requirements->getAllTests();
+
+$html = '<div class="check-table">';
+
+/** @var \QUI\Requirements\Tests\Test $Test */
+foreach ($allTests as $category => $Tests) {
+
+    // todo das kann man besser machen
+    if ($category == 'Datenbank' || $category == 'Database') {
+        continue;
     }
+
+    $html .= '<div class="system-check check-table-row">';
+    $html .= '<div class="check-table-col check-table-col-test">';
+    $html .= $category;
+    $html .= '</div>';
+    $html .= '<div class="check-table-col check-table-col-message">';
+    $html .= '<ul>';
+    foreach ($Tests as $Test) {
+
+        $test       = array();
+        $Result     = $Test->getResult();
+        $statusCode = $Result->getStatus();
+
+        switch ($Result->getStatus()) {
+            case \QUI\Requirements\TestResult::STATUS_OPTIONAL:
+            case \QUI\Requirements\TestResult::STATUS_OK:
+                $html .= '<li><span class="fa fa-check" title="';
+                break;
+
+            case \QUI\Requirements\TestResult::STATUS_FAILED:
+                $html .= '<li class="failed"><span class="fa fa-close" title="';
+
+                if ($tests['testsFailed'] === false) {
+                    $tests['testsFailed'] = true;
+                    $tests['icon']        = 'fa-close';
+                    $tests['status']      = 'failed';
+                }
+                break;
+
+            case \QUI\Requirements\TestResult::STATUS_UNKNOWN:
+            case \QUI\Requirements\TestResult::STATUS_WARNING:
+                $html .= '<li><span class="fa fa-exclamation-circle" title="';
+
+                if ($tests['testsFailed'] === false) {
+                    $tests['icon']   = 'fa-exclamation-circle';
+                    $tests['status'] = 'warning';
+                }
+                break;
+        }
+
+        $html .= $Result->getStatusHumanReadable() . '"></span>';
+        $html .= '<span class="test-name">' . $Test->getName() . '</span>';
+        $html .= '<div class="test-message">' . $Result->getMessage() . '</div>';
+
+    }
+    $html .= '</ul>';
+    $html .= '</div>';
+    $html .= '</div>';
 }
 
-if (empty($failedTests)) {
-    $failedTests = true;
-}
+$html .= '</div>';
 
-\QUI\Setup\Utils\Ajax::output($failedTests, 200);
+$tests['htmlResult'] = $html;
+
+
+\QUI\Setup\Utils\Ajax::output($tests, 200);
